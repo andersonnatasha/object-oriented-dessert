2b713d8f293d1547d3c2abebdac1149c7b414db6
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-12 20:09:20 +0000

    done through sell method, break for lunch

--- desserts.py
+++ desserts.py
@@ -4,10 +4,31 @@
 class Cupcake:
     """A cupcake."""
 
+    cache = {}
+
+    def __init__(self, name, flavor, price):
+        self.name = name 
+        self.flavor = flavor
+        self.price = price
+        self.qty = 0
+        self.cache[name] = [self.flavor, self.price, self.qty]
+
+    def add_stock(self, amount):
+        self.qty += amount
+
+    def sell(self, amount):
+        if self.qty == 0:
+            print('Sorry, these cupcakes are sold out')
+        elif self.qty - amount < 0:
+            print(f'Sorry, there are only {self.qty} of {self.name} available.')
+        else:
+            self.qty -= amount
+
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+    
 
 
 if __name__ == '__main__':
